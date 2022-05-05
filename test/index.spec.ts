c3117a047b2a6cd519ad69be493b9c95e9034392
Daniel Roe <daniel@roe.dev>
2022-05-05 15:58:07 +0100

    test: adopt vitest

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai'
+import { describe, expect, it } from 'vitest'
 
 import { basename, dirname, extname, format, parse, relative, delimiter, isAbsolute, join, normalize, resolve, sep, toNamespacedPath } from '../src'
 
