89e35adbdc6921b362cad6b67bbff30fb58ce73e
Pooya Parsa <pyapar@gmail.com>
2022-05-05 20:04:31 +0200

    fix!: avoid mixed named and default exports
    BREAKING CHANGE: `import pathe from 'pathe` should be changes to `import * as pathe from 'pathe'`

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-// Reference https://nodejs.org/api/path.html
-import type path from 'path'
-
-import * as _path from './path'
 export * from './path'
-
-// Default export
-export default {
-  ..._path
-} as Omit<typeof path, 'win32' | 'posix'>
