4d5482d51ab15238e928f06d66f1ca08a40eb3de
Daniel Roe <daniel@roe.dev>
2022-08-08 12:47:40 +0100

    fix: remove nuxt-specific aliases

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,6 @@ export function resolveAliases (_aliases: Record<string, string>) {
   // Resolve alias values in relation to each other
   for (const key in aliases) {
     for (const alias in aliases) {
-      if (!['~', '@', '#'].includes(alias[0])) { continue }
       if (alias === '@' && !aliases[key].startsWith('@/')) { continue } // Don't resolve @foo/bar
 
       if (aliases[key].startsWith(alias)) {
