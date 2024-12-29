fd2d35f0782397252e175f9796987519b979ec5d
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-29 13:01:17 +0800

    define `win32`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as path from "./path";
+
 export * from "./path";
 
-export * as default from "./path";
+export const win32 = path satisfies Omit<typeof import("node:path").win32, "posix" | "win32">;
+export default path;
