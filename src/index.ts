a3c1ef066e629dfc2764c05b872435117ebaef80
Pooya Parsa <pooya@pi0.io>
2024-12-30 15:15:06 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import type { PlatformPath } from "node:path";
+import type NodePath from "node:path";
+
 import * as path from "./path";
 
 export * from "./path";
 
-export const posix = path satisfies Omit<PlatformPath['posix'], "posix" | "win32">;
-export const win32 = path satisfies Omit<PlatformPath['win32'], "posix" | "win32">;
+export type Pathe = Omit<
+  typeof NodePath,
+  "posix" | "win32" | "matchesGlob" /* TODO */
+>;
 
-export default path;
+export const posix = path satisfies Pathe;
+
+export const win32 = path satisfies Pathe;
+
+export default path satisfies Pathe;
